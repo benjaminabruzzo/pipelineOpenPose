9ef55bef8a847d42952e9799f4d4358914b90ecc
benjamin@titan <abruzzo2@gmail.com>
2019-04-01 11:04:53 -0400

    installed ros

---
+++ README.md
@@ -0,0 +1,4 @@
+	mkdir -p ~/pipelineOP && cd ~/pipelineOP && git init
+	git remote add gh git@github.com:benjaminabruzzo/pipelineOpenPose.git && git pull gh master
+
+	
--- openpose_cfg/006_install_ros_kinetic.sh
+++ openpose_cfg/006_install_ros_kinetic.sh
@@ -12,13 +12,9 @@ rosdep update
 
 sudo apt-get install -y python3-yaml
 
-cd ~/pipelineOP/rospkg && git init && git remote add gh git@github.com:ros-infrastructure/rospkg.git && git pull gh master
-sudo python3 setup.py install
-
-
-# sudo apt-get install -y ros-kinetic-kobuki-desktop
-# sudo apt-get install -y ros-kinetic-multimaster-fkie ros-kinetic-multimaster-msgs
-
+cd ~/pipelineOP/rospkg && git init && git remote add gh git@github.com:ros-infrastructure/rospkg.git && git pull gh master && sudo python3 setup.py install
+cd ~/pipelineOP/catkin_pkg && git init && git remote add gh git@github.com:ros-infrastructure/catkin_pkg.git && git pull gh master sudo python3 setup.py install
+cd ~/pipelineOP/catkin && git init && git remote add gh git@github.com:ros/catkin.git && git pull gh kinetic-devel sudo python3 setup.py install
 
 echo ' ' >> ~/.bashrc
 echo '. ~/.ros/.bashros' >> ~/.bashrc
@@ -28,11 +24,4 @@ cp ~/pipeline16044/settings/.bashgazebo ~/.ros/
 
 mkdir -p ~/ros/src
 mkdir -p ~/ros/data/csv
-sudo apt-get install -y python-catkin-tools python-rosinstall python-rosinstall-generator python-wstool build-essential
 
-
-
-
-
-# sudo apt-get install -y ros-kinetic-urdf-sim-tutorial
-# sudo apt-get install -y ros-kinetic-ardrone-autonomy
