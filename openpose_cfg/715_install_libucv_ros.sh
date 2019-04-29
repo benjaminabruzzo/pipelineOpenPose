1d8353f639647002994a4adc836589f5c8beaf14
benjamin@titan <abruzzo2@gmail.com>
2019-04-29 09:06:17 -0400

    added udev rules

--- openpose_cfg/715_install_libucv_ros.sh
+++ openpose_cfg/715_install_libucv_ros.sh
@@ -10,3 +10,11 @@ make && sudo make install
 
 # Run catkin
 cd ~/ros/src/ && catkin build libuvc_ros
+
+# Add camera to udev rules
+# us lsusb to find the vendor id
+sudo touch /etc/udev/rules.d/99-uvc.rules
+echo "# UVC cameras" | sudo tee /etc/udev/rules.d/99-uvc.rules
+echo "# Bus 001 Device 003: ID 046d:082d Logitech, Inc. HD Pro Webcam C920" | sudo tee -a /etc/udev/rules.d/99-uvc.rules
+echo 'SUBSYSTEMS=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="082d", MODE="0666"' | sudo tee -a /etc/udev/rules.d/99-uvc.rules
+
